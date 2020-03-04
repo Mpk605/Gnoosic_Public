cebcff7e37c0cbf71f45fcd53861f82600afb8e8
ArthurGuinot <arthur.guinot66@gmail.com>
2020-03-04 12:23:35 +0100

    listener text changed

--- app/src/main/java/com/kinejou/gnoosic/FormActivity.java
+++ app/src/main/java/com/kinejou/gnoosic/FormActivity.java
@@ -3,14 +3,45 @@ package com.kinejou.gnoosic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class FormActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
 
+        final TextInputEditText fav1 = findViewById(R.id.fav_band_1);
+        fav1.addTextChangedListener(new TextWatcher() {
 
+            @Override
+            public void afterTextChanged(Editable s) {
+                Toast.makeText(FormActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //get list
+            }
+        });
+
+        findViewById(R.id.continue_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormActivity.this, fav1.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
