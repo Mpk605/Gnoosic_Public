2843b7bb777ac307630959b696b85f38f5e81276
ArthurGuinot <arthur.guinot66@gmail.com>
2020-03-04 11:23:04 +0100

    oklm

--- app/src/main/java/com/kinejou/gnoosic/MainActivity.java
+++ app/src/main/java/com/kinejou/gnoosic/MainActivity.java
@@ -3,6 +3,7 @@ package com.kinejou.gnoosic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //the 3 artists
+            }
+        });
+
+        findViewById(R.id.history).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Historic of liked artistes
+            }
+        });
     }
 }
